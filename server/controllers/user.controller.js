a82ac09f52c2407d8025dbde76948d7228290b79
kestsaurav21 <kestsaurav21@outlook.com>
2025-02-24 23:45:48 +0530

    feat: loginUser

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -31,4 +31,28 @@ exports.createUser = async (req, res) => {
 };
 
 
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Please provide email and password!' });
+        }
+        
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'Invalid Credentials!' });
+        }
 
+        const isPassswordMatching = await bcrypt.compare(password, user.password);
+        if (!isPassswordMatching) {
+            return res.status(401).json({ message: 'Invalid Credentials!' });
+        }
+
+        return res.status(200).json({ message: 'Login Successful!', user });
+        
+
+    } catch (error) {
+        
+        return res.status(500).json({ message: 'Error logging in user!', error: error.message });
+    }
+}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { createUser } = require('../controllers/user.controller');
+const { createUser, loginUser } = require('../controllers/user.controller');
 
 
 const router = express.Router();
 
 
 router.post('/signup', createUser);
+router.post('/login', loginUser);
 
 
 
