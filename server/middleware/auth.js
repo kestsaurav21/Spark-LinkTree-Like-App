41378eb927371e87aae8f3467f08a1784688f218
kestsaurav21 <kestsaurav21@outlook.com>
2025-02-25 00:27:37 +0530

    feat: auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+const isAuth = (req, res, next) => {
+    
+    if(req.headers.authorization){
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded; 
+            next();
+            
+        } catch (err) {
+            return res.status(401).json({ message: 'Invalid Token!' });
+        }
+    }else{
+        return res.status(401).json({ message: 'No Token!' });
+    }
+
+}
+
+module.exports = isAuth;
