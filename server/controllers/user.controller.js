00027e3e67e30c3d3b3f69f73e664c9b2e09562e
kestsaurav21 <kestsaurav21@outlook.com>
2025-02-25 00:50:09 +0530

    feat: getUser

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require("../models/user.model");
 
 exports.createUser = async (req, res) => {
@@ -48,7 +49,15 @@ exports.loginUser = async (req, res) => {
             return res.status(401).json({ message: 'Invalid Credentials!' });
         }
 
-        return res.status(200).json({ message: 'Login Successful!', user });
+        //Generate JWT Token
+        const token = jwt.sign(
+            {   id: user._id, email: user.email, username: user.username    },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        )
+
+
+        return res.status(200).json({ message: 'Login Successful!', user,  token });
         
 
     } catch (error) {
@@ -56,3 +65,19 @@ exports.loginUser = async (req, res) => {
         return res.status(500).json({ message: 'Error logging in user!', error: error.message });
     }
 }
+
+
+exports.getUser = async (req, res) => {
+    try {
+        const { id } = req.params.id;
+        const user = await User.findById({ _id : id });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found!' });
+        }
+        return res.status(200).json({ message: 'User found!', user });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+
